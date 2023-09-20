3798cad4a1ec828bb14cc48683f8080aed48de29
Tanha-013 <nusrattanha897@gmail.com>
2023-09-20 21:07:12 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl bg-orange-600 text-white text-center p-4 ">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
